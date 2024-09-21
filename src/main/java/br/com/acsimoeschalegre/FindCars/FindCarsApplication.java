8f7bce9458757c86d6cfff0fd0752a77114a290e
AC <acsimoes446@gmail.com>
2024-09-21 16:08:47 -0300

    initial commit

---
+++ src/main/java/br/com/acsimoeschalegre/FindCars/FindCarsApplication.java
@@ -0,0 +1,13 @@
+package br.com.acsimoeschalegre.FindCars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindCarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindCarsApplication.class, args);
+	}
+
+}
