93f0c76e01821152466dbf3a2a3bdbeba97250e3
AC <acsimoes446@gmail.com>
2024-09-21 21:07:51 -0300

    feat: add configuracao de opeapi

---
+++ src/main/java/br/com/acsimoeschalegre/FindCars/springdoc/OpenApiConfig.java
@@ -0,0 +1,30 @@
+package br.com.acsimoeschalegre.FindCars.springdoc;
+
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info()
+                        .title("FindVehicles")
+                        .description("API de procura de modelos de carros")
+                        .contact(new Contact()
+                                .name("Dev Backend")
+                                .email("acsimoes446@gmail.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://github.com/ceciliasimoes")));
+    }
+}
+
